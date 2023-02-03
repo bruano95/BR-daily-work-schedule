43454b0bba98eff9dd42ddfc67f1ba702519f4d0
bruano95 <bruano@uw.edu>
2023-02-02 22:42:40 -0800

    updated and working Java

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # BR-daily-work-schedule
+
+## Description
+
+This project allows users to set a scheduler for the current work day. The scheduler shows the current date at the top of the page and exhibits the hours within a work day. Here, users are able to enter in "To-Do" items and save using the save button located directly to the right of each time block. This information is then saved within the local storage. The time blocks change colors accordingly to the current time of the day. Past times turn grey, Present time is represented by the color red, and future times are represented by green.
+
+# Getting Started
+
+## Dependencies
+
+In order to access this web page a working device that has access to the internet and a proper web browser is necessary.
+
+## Installing
+
+If the items listed above are available, there are no additional installations needed to access this project.
+
+## Help
+
+If the project/ web page is not running properly, please contact me through the ways listed below.
+
+# Authors
+
+Brandon Ruano
+bruano@uw.edu
+github.com/bruano95
--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,53 @@
-//Date in header
-var currentDate = dayjs().format('dddd, D MMMM YYYY');
-$('#currentDay').text(currentDate);
+// Sets the Date for today
+var todaysDate = dayjs().format('dddd D MMMM YYYY');
+$('#currentDay').text(todaysDate);
 
-function currentTime() {
-  var timeOfDay = dayjs().hour();
+// sets the current time
+    function timeCurrently() {
 
-  $(".time-group").each(function () {
-    var groupTime = parseInt($(this).attr("id").split("-")[1]);
+      var currentTime = dayjs().hour();
 
-    if (groupTime === timeOfDay) {
-      $(this).removeClass("past");
-      $(this).removeClass("future");
-      $(this).addClass("present");
-    } else if (groupTime > timeOfDay) {
-      $(this).removeClass("past");
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    } else if (groupTime < timeOfDay) {
-      $(this).removeClass("present");
-      $(this).removeClass("future");
-      $(this).addClass("past");
+        $(".time-group").each(function () {
+       
+        var groupTime = parseInt($(this).attr("id").split("-")[1]);
+        // Changes the colors for past, present, and future times  
+        if (groupTime === currentTime) {
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+          $(this).addClass("present");
+        } else if (groupTime > currentTime) {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        } else if (groupTime < currentTime) {
+          $(this).removeClass("present");
+          $(this).removeClass("future");
+          $(this).addClass("past");
+          console.log(currentTime)
+        }
+      })
     }
-  })
-}
+    // takes inputed description of events to save within the local storage
+    $("#hour-09 .description").val(localStorage.getItem("hour-09"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
+// save button function, allows for button to work properly
 $(document).ready(function() {
   $('.saveBtn').on('click', function(event) {
   event.preventDefault();
    var text = $(this).siblings(".description").val();
    var time = $(this).parent().attr("id");
+   
+ // saves the inputted items into the local storage with time
+   localStorage.setItem(time,text);
+  })
 
-   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-   $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-   $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-   $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
-   currentTime();
+    timeCurrently();
+});
