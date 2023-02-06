6af7fbfe5a96f8b0d0c6234ed37a5989c1dba382
bruano95 <bruano@uw.edu>
2023-02-06 11:31:39 -0800

    changes to java

--- assets/script.js
+++ assets/script.js
@@ -27,16 +27,6 @@ $('#currentDay').text(todaysDate);
         }
       })
     }
-    // takes inputed description of events to save within the local storage
-    $("#hour-09 .description").val(localStorage.getItem("hour-09"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 // save button function, allows for button to work properly
 $(document).ready(function() {
